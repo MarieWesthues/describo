447b9b7a6de9064d8671d1a699145c65e90e9f99
MarieWesthues <marie.westhues@stud.uni-due.de>
2022-02-17 12:18:24 +0100

    button mit fragezeichen

--- describo-master/src/components/TargetSelection/LocalFolder.component.vue
+++ describo-master/src/components/TargetSelection/LocalFolder.component.vue
@@ -13,14 +13,12 @@
         </div>
         <div class="my-4 text-gray-600" style="font-style:italic">
             CHOOSE YOUR FOLDER OF A SPECIFIC RESEARCH PROJECT
-                    <el-button
-                        size="mini"
-                        type="primary"
-                        circle
-                        class=" focus:outline-none focus:border-2 focus:border-blue-600"
+                    <button
+    
+                        class="rounded-full bg-blue-400 w-6 h-6" style="color: white"
                     >
                         <i class="fas fa-question fa-fw"></i>
-                    </el-button> 
+                    </button> 
                
         </div>
     
